579cdd70886f72ab406dc7b90c12620e684f5cf9
Gabriel Nerys <biel.nerys150@gmail.com>
2023-02-28 19:55:15 -0300

    primeiro commit

---
+++ CalculoMedia/script.js
@@ -0,0 +1,14 @@
+function calcularMedia(){
+    var nota1 = parseFloat(document.getElementById("nota1").value);
+    var nota2 = parseFloat(document.getElementById("nota2").value);
+
+    var media = (nota1 + nota2)/2;
+
+    if(media >= 7){
+        alert("Parabens, aprovado! Media " +media);
+    }else{
+        alert("Reprovado! " +media);
+    }
+
+    var resultado = document.getElementById("resultado");
+}
